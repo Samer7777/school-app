e8156803c82051825f03d8c4eac50ac83b01aad9
samer majid <samerbahabsa@gmail.com>
2021-11-04 00:08:24 +0200

    done

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ defined('DATABASE_HOST_NAME')        ? null  : define('DATABASE_HOST_NAME', 'loc
 defined('DATABASE_USER_NAME')        ? null  : define('DATABASE_USER_NAME', 'root');
 defined('DATABASE_PASSWORD')         ? null  : define('DATABASE_PASSWORD', '');
 defined('DATABASE_DB_NAME')          ? null  : define('DATABASE_DB_NAME', 'school_db');
-defined('DOMAIN_NAME')               ? null  : define('DOMAIN_NAME', 'https://jarad.netlify.app');
+defined('DOMAIN_NAME')               ? null  : define('DOMAIN_NAME', 'https://samer7777.github.io/school-app/');
